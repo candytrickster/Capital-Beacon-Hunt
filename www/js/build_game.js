2e36a3cfe634012c567e44b4f5e5d1ac7afa9308
Tracie Wamsley <traciewamsley@Tracies-MacBook-Pro.local>
2017-08-16 14:01:52 -0600

    some transitions

--- www/js/build_game.js
+++ www/js/build_game.js
@@ -1,5 +1,17 @@
 function setupPlay() {
-	console.log("CLICKED!");
+	
+	var header = document.getElementById("header");
+	header.className += "gameStart";
+
+	var tagline = document.getElementById("tagline");
+	$("#tagline").fadeOut(function() {
+	  $(this).text("Found 0 of 10").fadeIn();
+	});
+	tagline.className += "tagline-play";
+
+	var canvas = document.getElementById("game");
+	canvas.className += "canvas-play";
+
 	stage.removeChild(playbtn, bee,bbee,sbee);
 	stage.update();
 }
