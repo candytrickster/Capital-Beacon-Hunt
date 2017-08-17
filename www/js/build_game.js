965ef964c4c84c25c25b5c59a71577de14aea759
Tracie Wamsley <traciewamsley@Tracies-MacBook-Pro.local>
2017-08-17 14:15:59 -0600

    hexagons in correct placement

--- www/js/build_game.js
+++ www/js/build_game.js
@@ -21,13 +21,42 @@ function setupPlay() {
 function setupItems() {
 	beacons[0] = new Item("Base Isolator", "0C:F3:EE:0D:9F:D4", false, 0, true, "This item helps our building stay up during earthquakes", "This item is by the wall safe in the visitor center");
 	beacons[1] = new Item("Liberty Bell", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
-	console.log(beacons[0]);
+	beacons[2] = new Item("Olene Walker", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[3] = new Item("House of Rep", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[4] = new Item("Senate", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[5] = new Item("Supreme Court", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[6] = new Item("Governor's Office", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[7] = new Item("Gold Room", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[8] = new Item("Filo", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
+	beacons[9] = new Item("Rotunda", "0C:F3:EE:0D:A4:4C", false, 0, true, "Clue", "hint");
 }
 
 function setupHexagons() {
 	for(item in beacons) {
-		hexagons[item] = new Hexagon(100+(item*100),100);
+		var x;
+
+		if(item == 0 || item%2 == 0){
+			x = (cwidth/2)-(cwidth/4);
+		} else {
+			x = (cwidth/2)+(cwidth/4);
+		}
+
+		hexagons[item] = new Hexagon(x,item);
 	}
+
+	// for(var i = 0; i < 9; i++){
+	// 	var x;
+
+	// 	if(i == 0 || i%2 == 0){
+	// 		x = (cwidth/2)-(cwidth/4);
+	// 	} else {
+	// 		x = (cwidth/2)+(cwidth/4);
+	// 	}
+	// 	hexagons[i] = new Hexagon(x,i);
+	// }
+
+
+	// hexagons[0] = new Hexagon(100,0);
 
 	for(hex in hexagons){
 		stage.addChild(hexagons[hex]);
--- www/js/build_grid.js
+++ www/js/build_grid.js
@@ -1,8 +1,17 @@
-function Hexagon(x, y)
+function Hexagon(x, index)
 {
 	var hex = new createjs.Shape();
 	hex.graphics.beginStroke("#FFC700");
 	hex.graphics.setStrokeStyle(10);
-	hex.graphics.beginFill("#FFDF00").drawPolyStar(x, y, 50, 6, 0, 30);
+	var size = (cwidth/2)-(cwidth/3.5);
+	var y= 290;
+	
+	if(index % 2 == 0 && index != 0) {
+		y = y+((size*2)*index);
+	} else if(index != 0 && index != 1) {
+		y = y+((size*2)*(index-1));
+	}
+
+	hex.graphics.beginFill("#FFDF00").drawPolyStar(x, y, size, 6, 0, 30);
 	return hex;
 }
